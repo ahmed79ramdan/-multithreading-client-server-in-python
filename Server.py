bd41dd3f6ce9c6632d287bb9927eed5345ad9d15
ahmed79ramdan <77209823+ahmed79ramdan@users.noreply.github.com>
2022-12-17 14:44:32 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,3 +1,7 @@
+# This is a small example of socket programming that is able to connect multiple clients to a server using python 3 sockets.
+# It can send messages from clients to server, and from server to clients.
+# This example also shows how to host the socket server locally or globally across the internet so anyone can connect.
+# This uses the python 3 socket and threading module.
 import socket
 import threading
 import pickle                          #inorder to send other data type
